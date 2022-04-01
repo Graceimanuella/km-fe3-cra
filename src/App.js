eb3b97b27b9d1f90ba037910280e213fac2ce04e
Grace Imanuela <graceimanuella28@gmail.com>
2022-04-01 11:57:47 +0700

    Homework 8

--- src/App.js
+++ src/App.js
@@ -108,13 +108,23 @@ function App() {
       <div>
         {tracks.map((item) => {
           return (
-            <Track
-              key={item.album.id}
-              image_url={item.album.images[0].url}
-              track_title={item.name}
-              artist_name={item.album.artists[0].name}
-              album_name={item.album.name}
-            />
+            <>
+              <Track
+                key={item.album.id}
+                image_url={item.album.images[0].url}
+                track_title={item.name}
+                artist_name={item.album.artists[0].name}
+                album_name={item.album.name}
+              />
+
+              
+              <button id="demo" onclick="myFunction()">Select</button>
+              <script>
+                function myFunction() {
+                  document.getElementById("demo").innerHTML = "Deselect"
+                }
+              </script>
+            </>            
           );
         })}
       </div>
